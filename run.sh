05e9014d070b9b12f7fdf4ab8eaa4853bb5e55b4
manuel alves <manuelalves@manuels-MacBook-Pro.local>
2015-10-11 17:31:51 +0100

    clean run.sh file

--- run.sh
+++ run.sh
@@ -7,10 +7,13 @@
 rm -f *.pdf
 rm -f *.fst
 
-################### letras ################
+################### transdutores individuais ################
 
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  digitos1a9.txt  > digitos1a9.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt digitos1a9.fst | dot -Tpdf > digitos1a9.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  digitos3a9horas.txt  > digitos3a9horas.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt digitos3a9horas.fst | dot -Tpdf > digitos3a9horas.pdf
 
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  digitos10a19.txt  > digitos10a19.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt digitos10a19.fst | dot -Tpdf > digitos10a19.pdf
@@ -21,6 +24,15 @@ fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt digitos20a50.fst | dot 
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  doispontos.txt  > doispontos.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt doispontos.fst | dot -Tpdf > doispontos.pdf
 
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  doze.txt  > doze.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt doze.fst | dot -Tpdf > doze.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  duas.txt  > duas.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt duas.fst | dot -Tpdf > duas.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  e.txt  > e.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt e.fst | dot -Tpdf > e.pdf
+
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  emeia.txt  > emeia.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt emeia.fst | dot -Tpdf > emeia.pdf
 
@@ -30,44 +42,8 @@ fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt meiodia.fst | dot -Tpdf
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  paraas.txt  > paraas.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt paraas.fst | dot -Tpdf > paraas.pdf
 
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  umquarto.txt  > umquarto.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt umquarto.fst | dot -Tpdf > umquarto.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  zero.txt  > zero.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt zero.fst | dot -Tpdf > zero.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  digitos3a9horas.txt  > digitos3a9horas.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt digitos3a9horas.fst | dot -Tpdf > digitos3a9horas.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  uma.txt  > uma.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt uma.fst | dot -Tpdf > uma.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  duas.txt  > duas.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt duas.fst | dot -Tpdf > duas.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  zerohoras.txt  > zerohoras.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt zerohoras.fst | dot -Tpdf > zerohoras.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  vinte.txt  > vinte.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt vinte.fst | dot -Tpdf > vinte.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  vinteeum.txt  > vinteeum.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt vinteeum.fst | dot -Tpdf > vinteeum.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  e.txt  > e.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt e.fst | dot -Tpdf > e.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  doze.txt  > doze.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt doze.fst | dot -Tpdf > doze.pdf
-
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  quinze.txt  > quinze.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt quinze.fst | dot -Tpdf > quinze.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  um.txt  > um.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt um.fst | dot -Tpdf > um.pdf
-
-fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  tresquartos.txt  > tresquartos.fst
-fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt tresquartos.fst | dot -Tpdf > tresquartos.pdf
 
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  teste1.txt  > teste1.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt teste1.fst | dot -Tpdf > teste1.pdf
@@ -78,37 +54,57 @@ fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt teste2.fst | dot -Tpdf 
 fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  teste3.txt  > teste3.fst
 fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt teste3.fst | dot -Tpdf > teste3.pdf
 
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  tresquartos.txt  > tresquartos.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt tresquartos.fst | dot -Tpdf > tresquartos.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  um.txt  > um.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt um.fst | dot -Tpdf > um.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  uma.txt  > uma.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt uma.fst | dot -Tpdf > uma.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  umquarto.txt  > umquarto.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt umquarto.fst | dot -Tpdf > umquarto.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  vinte.txt  > vinte.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt vinte.fst | dot -Tpdf > vinte.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  vinteeum.txt  > vinteeum.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt vinteeum.fst | dot -Tpdf > vinteeum.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  zero.txt  > zero.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt zero.fst | dot -Tpdf > zero.pdf
+
+fstcompile --isymbols=data.sym.txt --osymbols=data.sym.txt  zerohoras.txt  > zerohoras.fst
+fstdraw  --isymbols=data.sym.txt --osymbols=data.sym.txt zerohoras.fst | dot -Tpdf > zerohoras.pdf
+
 #################### Compila e gera a versão gráfica do transdutor das horas ####################
 
-####### 1 #######
+####### 0 e 1 #######
 fstunion zerohoras.fst uma.fst > hora0e1.fst
-fstdraw --isymbols=data.sym.txt --osymbols=data.sym.txt hora0e1.fst | dot -Tpdf > hora0e1.pdf
 
-####### 2 #######
+####### 0 a 2 #######
 fstunion hora0e1.fst duas.fst > hora0a2.fst
-fstrmepsilon hora0a2.fst hora0a2.fst
-fstdraw --isymbols=data.sym.txt --osymbols=data.sym.txt hora0a2.fst | dot -Tpdf > hora0a2.pdf
+fstrmepsilon hora0a2.fst hora0a2.fst                             # limpa os eps desnecessarios
 
-####### 3 - 9 #######
+####### 0 a 9 #######
 fstunion hora0a2.fst digitos3a9horas.fst > hora0a9.fst
 fstconcat zero.fst hora0a9.fst > 0a9horas.fst
-fstrmepsilon 0a9horas.fst 0a9horas.fst
-fstdraw --isymbols=data.sym.txt --osymbols=data.sym.txt 0a9horas.fst | dot -Tpdf > 0a9horas.pdf
+fstrmepsilon 0a9horas.fst 0a9horas.fst                          # limpa os eps desnecessarios
 
-####### 10 - 19 #######
+####### 0 a 19 #######
 fstunion  digitos10a19.fst meiodia.fst > digitos10a19w12.fst    # adiciona meio dia a lista 10 a 19
 fstunion  digitos10a19w12.fst quinze.fst > digitos10a19w15.fst  # adiciona quinze a lista anterior
 fstconcat um.fst digitos10a19w15.fst > digitos10a19w1.fst       # adiciona o primeiro digito 1_ a lista anterior
 fstunion 0a9horas.fst digitos10a19w1.fst > hora0a19.fst
-fstrmepsilon hora0a19.fst hora0a19.fst
-fstdraw --isymbols=data.sym.txt --osymbols=data.sym.txt hora0a19.fst | dot -Tpdf > hora0a19.pdf
+fstrmepsilon hora0a19.fst hora0a19.fst                          # limpa os eps desnecessarios
 
-####### 20 #######
+####### 0 a 24 #######
 fstunion zero.fst vinteeum.fst > zeroe21.fst
-fstrmepsilon zeroe21.fst zeroe21.fst
+fstrmepsilon zeroe21.fst zeroe21.fst                              # limpa os eps desnecessarios
 fstconcat vinte.fst zeroe21.fst > vinte20.fst
 fstunion hora0a19.fst vinte20.fst > horas.fst
-fstrmepsilon horas.fst horas.fst
+fstrmepsilon horas.fst horas.fst                                  # limpa os eps desnecessarios
 fstdraw --isymbols=data.sym.txt --osymbols=data.sym.txt horas.fst | dot -Tpdf > horas.pdf
 
 #################### Compila e gera a versão gráfica do transdutor dos minutos ####################
@@ -118,27 +114,27 @@ fstunion zero.fst digitos1a9.fst  > minutos0a9aux.fst             # introduz o 0
 fstconcat zero.fst minutos0a9aux.fst  > minutos0a9.fst            # zero no primeiro digito 0_
 
 ####### 20 - 59 #######
-fstunion zero.fst digitos1a9.fst > digitos1a9ou0.fst              # zero no segundo digito no caso de 20 e 50
-fstconcat digitos20a50.fst digitos1a9ou0.fst > minutos20a50.fst   # os restantes 9 digitos do segundo digito para numeros de 20-59
-fstunion emeia.fst minutos20a50.fst > minutos20a50w30.fst
-fstunion tresquartos.fst minutos20a50w30.fst > minutos20a50w40.fst
-fstconcat e.fst minutos20a50w40.fst > minutos20a50we.fst
+fstunion zero.fst digitos1a9.fst > digitos1a9ou0.fst                # zero no segundo digito no caso de 20 e 50
+fstconcat digitos20a50.fst digitos1a9ou0.fst > minutos20a50.fst     # os restantes 9 digitos do segundo digito para numeros de 20-59
+fstunion emeia.fst minutos20a50.fst > minutos20a50w30.fst           # junta os 30 minutos a lista anterior
+fstunion tresquartos.fst minutos20a50w30.fst > minutos20a50w40.fst  # junta os 45 minutos a lista anterior
+fstconcat e.fst minutos20a50w40.fst > minutos20a50we.fst            # adiciona o "e" antes da lista anterior
 
 ####### 0 - 59 #######
 fstunion  digitos10a19.fst doze.fst > digitos10a19w12.fst         # adiciona doze a lista 10 a 19
 fstunion  digitos10a19w12.fst umquarto.fst > digitos10a19w15.fst  # adiciona um quarto a lista anterior
 fstconcat um.fst digitos10a19w15.fst > digitos10a19w1.fst         # adiciona o primeiro digito 1_ a lista anterior
 fstconcat e.fst digitos10a19w1.fst > edigitos10a19.fst            # adiciona a letra e antes da lista anterior
-fstunion edigitos10a19.fst minutos20a50we.fst > minutosaux.fst      # junta os numeros de 10-19 com os de 20-50
-fstunion minutosaux.fst minutos0a9.fst > minutos.fst              # Junta os valores de 0-9 aos restantes
+fstunion edigitos10a19.fst minutos20a50we.fst > minutosaux.fst    # junta os numeros de 10-19 com os de 20-50
+fstunion minutosaux.fst minutos0a9.fst > minutos.fst              # junta os valores de 0-9 aos restantes
 fstrmepsilon minutos.fst minutos.fst                              # limpa os eps desnecessarios
 fstdraw --isymbols=data.sym.txt --osymbols=data.sym.txt minutos.fst | dot -Tpdf > minutos.pdf
 
 #################### Compila e gera a versão gráfica do transdutor relogio ####################
 
-fstconcat horas.fst doispontos.fst > horasdoispontos.fst
-fstconcat horasdoispontos.fst minutos.fst > relogio.fst
-fstrmepsilon relogio.fst relogio.fst
+fstconcat horas.fst doispontos.fst > horasdoispontos.fst          # coloca os : depois das horas
+fstconcat horasdoispontos.fst minutos.fst > relogio.fst           # adiciona os minutos
+fstrmepsilon relogio.fst relogio.fst                              # limpa os eps desnecessarios
 fstdraw --isymbols=data.sym.txt --osymbols=data.sym.txt relogio.fst | dot -Tpdf > relogio.pdf
 
 #################### Testes aos Transfutores
